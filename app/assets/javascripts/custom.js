71184ac0db46cebdd6564bdf4d7ec586d235f5fc
Alexey <avaelkross@gmail.com>
2014-07-28 00:01:02 +0600

    remove item limit and fool styles

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,4 @@
 $( document ).ready(function(){
-  var user_phones_list = new List('user_phones_list', {valueNames: [ 'phone', 'condition', 'type', 'selling' ]});
-  var phones_list = new List('phones_list', {valueNames: ['name']})
+  var user_phones_list = new List('user_phones_list', {valueNames: [ 'phone', 'condition', 'type', 'selling' ], page: 1000});
+  var phones_list = new List('phones_list', {valueNames: ['name'], page: 1000})
 });
